5318aadce8a54ff2cc33f0cecb9773c19aabaecb
olga <oluciams84@gmail.com>
2022-03-14 16:22:52 -0500

    Add component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getCharacterApi } from '../utils/api';
 import {ListImage} from './ListImage';
+import { BtnPagination } from './BtnPagination';
 
 const axios = require('axios');
 
@@ -73,6 +74,7 @@ export const Home = ()=>{
         </div>     
       </section> 
       <section className="row row-cols-sm-2 row-cols-md-4">
+
         {characters
 
           .filter(character => character.name.match(new RegExp(searchCharacter, "i")))
@@ -87,27 +89,11 @@ export const Home = ()=>{
         }
       </section>
       <section className="mb-5">
-        <div className="d-flex justify-content-start gap-3">
-          <div>                             
-            <button type="button" className="btn btn-outline-dark fw-bold" id="prev" onClick={() => fetchData('prev')}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-              </svg>
-              Prev
-            </button>
-          </div>
-          <div className="align-items-center fs-5 fw-bold mt-1">
-            <p>{numPage} of {pages}</p>       
-          </div> 
-          <div>                  
-            <button type="button" className="btn btn-outline-dark fw-bold" id="next" onClick={()=>fetchData('next')}>
-              Next
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-              </svg>
-            </button>
-          </div>       
-        </div>
+        <BtnPagination
+          fetchData={fetchData}
+          pages={pages}
+          numPage={numPage}
+        />
       </section>
     </div>
   )
--- src/components/ListImage.js
+++ src/components/ListImage.js
@@ -3,7 +3,6 @@ import React from 'react';
 export const ListImage = ({image, nameCharacter})=>{ 
 
   return(
-
     <div className="mt-3 mb-3">
       <div className="card" style={{width: "18rem"}}>
         <img src={image} className="card-img-top" alt="character"/>
@@ -11,8 +10,6 @@ export const ListImage = ({image, nameCharacter})=>{
           <h5 className="card-text">{nameCharacter}</h5>        
         </div>
       </div>
-    </div>
-     
-    
+    </div>  
   )
 }
